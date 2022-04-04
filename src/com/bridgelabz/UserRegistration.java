b6c56c69170cd8c8ed7c6da0301cfb3fcb9ab483
Mahesh <mahesh51138@gmail.com>
2022-04-04 20:01:48 +0530

    refactored:Test Cases and methods can handle the  inputs

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,39 +1,82 @@
 package com.bridgelabz;
+//purpose: here these class verify user registration using Java Regular Expressions
+//          and Patterns  with valid first name,valid last name,valid email,
+//          valid password with different charactersand valid phone number
+
+
+//Auther: mahesh babu
 
 public class UserRegistration {
+/**
+ * this method is to verify or validate the user first name
+ * @param str
+ */
+
     public boolean validationOfFirstName(String str) {
         String regex = "^[A-Z]+[a-z]{2,}";
         return str.matches(regex);
     }
+    /**
+     * this method is to verify the user entered last name
+     * @param str
+     */
+
     public boolean validationOfLastName(String str) {
         String regex = "^[A-Z]+[a-z]{2,}";
         return str.matches(regex);
     }
+    /**
+     * this method is to verify the user entered emailId
+     * @param emailId
+     */
     public boolean validationOfEmailId(String emailId) {
         String regex = "^[A-Za-z0-9-+._]+@[a-z0-9]+(.[a-z]{2,3})+$";
         return emailId.matches(regex);
     }
+    /**
+     * this method is to check the user entered mobile number
+     * @param mobileNumber
+     */
     public boolean validationOfMobileNumber(String mobileNumber) {
         String regex = "^[0-9]{2}+ +[0-9]{10}$";
         return mobileNumber.matches(regex);
     }
+    /**
+     * this is to check the password with minimum eight characters
+     * @param password
+     */
     public boolean validationOfPasswordRuleOne(String password) {
         String regex = "^[A-Za-z0-9.-_@#$%&^*()+:?]{8,}$";
         return (password.matches(regex));
     }
+    /**
+     * this method is to validate the password with atleast one Upper case
+     * @param password
+     */
     public boolean validationOfPasswordRuleTwo(String password) {
         String regex = "^(?=.*[A-Z])[A-Za-z0-9._@#$%&^*()+:?]{8,}$";
         return (password.matches(regex));
     }
+    /**
+     * this method is to validate password with atleast one number
+     * @param password
+     */
     public boolean validationOfPasswordRuleThree(String password) {
         String regex = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9._@#$%&^*()+:?]{8,}$";
         return (password.matches(regex));
     }
+    /**
+     * this method is to validate password with Exactly one special character
+     * @param password
+     */
     public boolean validationOfPasswordRuleFour(String password) {
         String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[._@#$%&^*()+:?])[A-Za-z0-9._@#$%&^*()+:?]{8,}$";
         return (password.matches(regex));
     }
-
+    /**
+     * this method is to validate user email sample provide saparately
+     * @param userDetails
+     */
     public String validationOfAllDetails(UserDetails userDetails) {
         if ((this.validationOfFirstName(userDetails.firstName)) && (this.validationOfLastName(userDetails.lastName))
                 && (this.validationOfEmailId(userDetails.email)) && (this.validationOfMobileNumber(userDetails.mobile)) &&
--- src/com/bridgelabz/UserRegistrationTest.java
+++ src/com/bridgelabz/UserRegistrationTest.java
@@ -4,86 +4,154 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+//purpose:here these class verify  JUnit Test Methods
+//          and Parameterised Test Method for following
+//            user regisration probles
+
+//Auther: mahesh babu
 public class UserRegistrationTest {
+    /**
+     * this method is to verify or validate the user first name
+     * @return true
+     */
+
     @Test
     void givenFirstNameWithFirstLetterCapitalMInThreeCharactersReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfFirstName("Mahesh"));
     }
+    /**
+     * this method is to verify or validate the user first name
+     * @return  false
+     */
 
     @Test
     void givenFirstNameWithOutFirstLetterCapitalMInThreeCharactersReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertFalse(userRegistration.validationOfFirstName("bhavi"));
+        Assertions.assertFalse(userRegistration.validationOfFirstName("mahesh"));
     }
+    /**
+     * this method is to verify or validate the user last name
+     * @return true
+     */
     @Test
-    void givenLastWithFirstLetterCapitalMInThreeCharactersReturnsTrue() {
+    void givenLasNameWithFirstLetterCapitalMInThreeCharactersReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfLastName("Mahesh"));
     }
-
+    /**
+     * this method is to verify or validate the user last name
+     * @return  false
+     */
     @Test
     void givenLastNameWithOutFirstLetterCapitalMInThreeCharactersReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertFalse(userRegistration.validationOfLastName("bhavi"));
+        Assertions.assertFalse(userRegistration.validationOfLastName("mahesh"));
     }
-
+    /**
+     * this method is to verify the user entered emailId
+     * @return true
+     */
     @Test
     void givenEmailIdValidatingWithRegexReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfEmailId("maheshbabu12@gmail.com"));
     }
+    /**
+     * this method is to verify the user entered emailId
+     * @return false
+     */
     @Test
     void givenEmailIdValidatingWithRegexReturnsFalse(){
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertFalse(userRegistration.validationOfEmailId("maheshbabu12@@gmail.com"));
     }
+    /**
+     * this is to check the password with minimum eight characters
+     * @return true
+     */
     @Test
     void givenMobileNumberValidatingWithRegexReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfMobileNumber("91 8353696252"));
     }
-
+    /**
+     * this is to check the password with minimum eight characters
+     * @return false
+     */
     @Test
     void givenMobileNumberValidatingWithRegexReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertFalse(userRegistration.validationOfMobileNumber("918353696252"));
     }
+    /**
+     * this is to check the password with minimum eight characters
+     * @return true
+     */
     @Test
     void givenPasswordValidatingWithMinEightCharactersReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        Assertions.assertTrue(userRegistration.validationOfPasswordRuleOne("mahes123"));
+        Assertions.assertTrue(userRegistration.validationOfPasswordRuleOne("Mahes123"));
     }
+    /**
+     * this is to check the password with minimum eight characters
+     * @return false
+     */
     @Test
     void givenPasswordValidatingWithMinEightCharactersReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertFalse(userRegistration.validationOfPasswordRuleOne("mah123"));
     }
+
     @Test
+    /**
+     * this method is to validate the password with atleast one Upper case
+     * @return true
+     */
     void givenPasswordValidatingWithAtleastOneCapitalLetterReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfPasswordRuleTwo("Mahesh123"));
     }
+    /**
+     * this method is to validate the password with atleast one Upper case
+     * @return false
+     */
     @Test
     void givenPasswordValidatingWithoutOneCapitalLetterReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertFalse(userRegistration.validationOfPasswordRuleTwo("mahesh123"));
     }
+    /**
+     * this method is to validate password with atleast one number
+     * @return true
+     */
     @Test
     void givenPasswordValidatingWithAtLeastOneNumberReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfPasswordRuleThree("Mahesh123"));
     }
+    /**
+     * this method is to validate password with atleast one number
+     * @return false
+     */
     @Test
     void givenPasswordValidatingWithoutOneNumberReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertFalse(userRegistration.validationOfPasswordRuleThree("maheshmahesh"));
     }
+    /**
+     * this method is to validate password with Exactly one special character
+     * @return true
+     */
     @Test
     void givenPasswordValidatingWithAtLeastOneSpecialCharacterReturnsTrue() {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfPasswordRuleFour("Mahesh123@"));
     }
+    /**
+     * this method is to validate password with Exactly one special character
+     * @return false
+     */
     @Test
     void givenPasswordValidatingWithoutOneSpecialCharacterReturnsFalse() {
         UserRegistration userRegistration = new UserRegistration();
@@ -96,12 +164,20 @@ public class UserRegistrationTest {
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertTrue(userRegistration.validationOfEmailId(str));
     }
+    /**
+     * this method is to validate email sample provide saparately
+     * @return true
+     */
     @Test
     void givenUserDetailsShouldReturnHappy(){
         UserDetails userDetails = new UserDetails("Mahesh","Babu","maheshbabu12@gmail.com","91 9666069691","9966988298Ki@");
         UserRegistration userRegistration = new UserRegistration();
         Assertions.assertEquals("Happy",userRegistration.validationOfAllDetails(userDetails));
     }
+    /**
+     * this method is to validate email sample provide saparately
+     * @return false
+     */
     @Test
     void givenUserDetailsShouldReturnSad(){
         UserDetails userDetails = new UserDetails("Mahesh","babu","maheshbabu12@gmail.com","91 9666569291","9966988298Ki@");
